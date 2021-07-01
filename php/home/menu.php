b1dad77e9c63d0b566b3aba32efc798129c250c5
Ahsanul Hoque <hoque.talha@gmail.com>
2021-07-01 17:22:33 +0600

    $commitMsg

--- php/home/menu.php
+++ php/home/menu.php
@@ -37,7 +37,7 @@
                                 <?php foreach ($categories as $category) {
                                     
                                 ?>
-                                    $categoryName = '$category->name';
+                                    $categoryName = <?php echo $categoryName; ?>;
                                     <li class=" <?php echo  $categoryName == 'Rice' ? 'active' : ''; ?>"><a href="#<?php echo $categoryName; ?>" data-toggle="tab"><?php echo $categoryName; ?></a></li>
                                 <?php } ?>
                             </ul>
