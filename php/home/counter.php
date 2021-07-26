f4cbaae3234105b9ba240793f8ed620ff8ae35c8
Ahsanul Hoque <hoque.talha@gmail.com>
2021-07-26 14:10:53 +0600

    $commitMsg

--- php/home/counter.php
+++ php/home/counter.php
@@ -8,29 +8,29 @@
                             <ul class="sm-counter-nav">
                                 <li class="col-md-3 col-sm-3 col-xs-12">
                                     <div class="sm-single-counter">
-                                        <span>Fresh</span>
-                                        <h3><span class="counter">55</span><sup>+</sup></h3>
-                                        <p>Breakfast Items</p>
+                                        <!-- <span>Fresh</span> -->
+                                        <h3><span class="counter">5</span><sup>+</sup></h3>
+                                        <p>Rice Items</p>
                                     </div>
                                 </li>
                                 <li class="col-md-3 col-sm-3 col-xs-12">
                                     <div class="sm-single-counter">
                                         <span>Delicious</span>
-                                        <h3><span class="counter">150</span><sup>+</sup></h3>
-                                        <p>Lunch Items</p>
+                                        <h3><span class="counter">10</span><sup>+</sup></h3>
+                                        <p>Curry Items</p>
                                     </div>
                                 </li>
                                 <li class="col-md-3 col-sm-3 col-xs-12">
                                     <div class="sm-single-counter">
-                                        <span>Hot</span>
-                                        <h3><span class="counter">35</span><sup>+</sup></h3>
-                                        <p>Coffee Items</p>
+                                        <!-- <span>Hot</span> -->
+                                        <h3><span class="counter">10</span><sup>+</sup></h3>
+                                        <p>Side Items</p>
                                     </div>
                                 </li>
                                 <li class="col-md-3 col-sm-3 col-xs-12">
                                     <div class="sm-single-counter">
                                         <span>Satisfied</span>
-                                        <h3><span class="counter">3562</span><sup>+</sup></h3>
+                                        <h3><span class="counter">120</span><sup>+</sup></h3>
                                         <p>Customers</p>
                                     </div>
                                 </li>
