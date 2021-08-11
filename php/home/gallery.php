5e1324b804918aaeb74e6064c46cef4542759641
Ahsanul Hoque <hoque.talha@gmail.com>
2021-08-11 17:22:12 +0600

    $commitMsg

--- php/home/gallery.php
+++ php/home/gallery.php
@@ -11,145 +11,78 @@
                             <span class="sm-title-bar"></span>
                         </div>
                         <div class="sm-gallery-content">
-                            <!-- <div class="sm-gallery-top">
-                                <!-- Start gallery menu -->
-                                <ul>
-                                    <li class="filter active" data-filter="all">ALL</li>
-                                    <li class="filter" data-filter=".food">FOOD</li>
-                                    <li class="filter" data-filter=".drink">DRINK</li>
-                                    <li class="filter" data-filter=".restaurant">RESTAURANT</li>
-                                    <li class="filter" data-filter=".dinner">DINNER</li>
-                                    <li class="filter" data-filter=".dessert">DESSERT</li>
-                                </ul>
-                            </div> -->
-                            <!-- Start gallery image -->
-                            <div class="sm-gallery-body" id="mixit-container">
-                                <!-- start single gallery image -->
-                                <div class="sm-single-gallery col-md-4 mix food">
-                                    <div class="sm-single-gallery-item">
-                                        <figure class="sm-single-gallery-img">
-                                            <a href="#"><img alt="img" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/sm-gallery1.jpg"></a>
-                                        </figure>
-                                        <div class="sm-single-gallery-info">
-                                            <a href="assets/img/gallery/big/1.jpg" data-fancybox-group="gallery" class="fancybox">
-                                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/plus.png" alt="plus icon img">
-                                            </a>
-                                        </div>
-                                    </div>
+
+                            <div class="responsive">
+                                <div class="gallery">
+                                    <a target="_blank" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/1.jpg">
+                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/1.jpg"> alt="food" width="600" height="400">
+                                    </a>
+                                    <div class="desc">Rice</div>
                                 </div>
-                                <!-- End single gallery image -->
-                                <!-- start single gallery image -->
-                                <div class="sm-single-gallery col-md-4 mix drink">
-                                    <div class="sm-single-gallery-item">
-                                        <figure class="sm-single-gallery-img">
-                                            <a href="#"><img alt="img" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/sm-gallery2.jpg"></a>
-                                        </figure>
-                                        <div class="sm-single-gallery-info">
-                                            <a href="assets/img/gallery/big/2.jpg" data-fancybox-group="gallery" class="fancybox">
-                                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/plus.png" alt="plus icon img">
-                                            </a>
-                                        </div>
-                                    </div>
+                            </div>
+
+
+                            <div class="responsive">
+                                <div class="gallery">
+                                    <a target="_blank" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/2.jpg">
+                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/2.jpg"> alt="food" width="600" height="400">
+                                    </a>
+                                    <div class="desc">Mutton Rezala</div>
                                 </div>
-                                <!-- End single gallery image -->
-                                <!-- start single gallery image -->
-                                <div class="sm-single-gallery col-md-4 mix restaurant">
-                                    <div class="sm-single-gallery-item">
-                                        <figure class="sm-single-gallery-img">
-                                            <a href="#"><img alt="img" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/sm-gallery3.jpg"></a>
-                                        </figure>
-                                        <div class="sm-single-gallery-info">
-                                            <a href="assets/img/gallery/big/3.jpg" data-fancybox-group="gallery" class="fancybox">
-                                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/plus.png" alt="plus icon img">
-                                            </a>
-                                        </div>
-                                    </div>
+                            </div>
+
+                            <div class="responsive">
+                                <div class="gallery">
+                                    <a target="_blank" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/3.jpg">
+                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/3.jpg"> alt="food" width="600" height="400">
+                                    </a>
+                                    <div class="desc">Chicken Rezala</div>
                                 </div>
-                                <!-- End single gallery image -->
-                                <!-- start single gallery image -->
-                                <div class="sm-single-gallery col-md-4 mix dinner">
-                                    <div class="sm-single-gallery-item">
-                                        <figure class="sm-single-gallery-img">
-                                            <a href="#"><img alt="img" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/sm-gallery4.jpg"></a>
-                                        </figure>
-                                        <div class="sm-single-gallery-info">
-                                            <a href="assets/img/gallery/big/4.jpg" data-fancybox-group="gallery" class="fancybox">
-                                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/plus.png" alt="plus icon img">
-                                            </a>
-                                        </div>
-                                    </div>
+                            </div>
+
+                            <div class="responsive">
+                                <div class="gallery">
+                                    <a target="_blank" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/4.jpg">
+                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/4.jpg"> alt="food" width="600" height="400">
+                                    </a>
+                                    <div class="desc">Salaad</div>
                                 </div>
-                                <!-- End single gallery image -->
-                                <!-- start single gallery image -->
-                                <div class="sm-single-gallery col-md-4 mix dinner">
-                                    <div class="sm-single-gallery-item">
-                                        <figure class="sm-single-gallery-img">
-                                            <a href="#"><img alt="img" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/sm-gallery5.jpg"></a>
-                                        </figure>
-                                        <div class="sm-single-gallery-info">
-                                            <a href="assets/img/gallery/big/5.jpg" data-fancybox-group="gallery" class="fancybox">
-                                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/plus.png" alt="plus icon img">
-                                            </a>
-                                        </div>
-                                    </div>
+                            </div>
+                            <div class="responsive">
+                                <div class="gallery">
+                                    <a target="_blank" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/5.jpg">
+                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/5.jpg"> alt="food" width="600" height="400">
+                                    </a>
+                                    <div class="desc">Tikka kabab</div>
                                 </div>
-                                <!-- End single gallery image -->
-                                <!-- start single gallery image -->
-                                <div class="sm-single-gallery col-md-4 mix food">
-                                    <div class="sm-single-gallery-item">
-                                        <figure class="sm-single-gallery-img">
-                                            <a href="#"><img alt="img" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/sm-gallery6.jpg"></a>
-                                        </figure>
-                                        <div class="sm-single-gallery-info">
-                                            <a href="assets/img/gallery/big/6.jpg" data-fancybox-group="gallery" class="fancybox">
-                                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/plus.png" alt="plus icon img">
-                                            </a>
-                                        </div>
-                                    </div>
+                            </div>
+
+
+                            <div class="responsive">
+                                <div class="gallery">
+                                    <a target="_blank" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/6.jpg">
+                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/6.jpg"> alt="food" width="600" height="400">
+                                    </a>
+                                    <div class="desc">Borhani</div>
                                 </div>
-                                <!-- End single gallery image -->
-                                <!-- start single gallery image -->
-                                <div class="sm-single-gallery col-md-4 mix drink">
-                                    <div class="sm-single-gallery-item">
-                                        <figure class="sm-single-gallery-img">
-                                            <a href="#"><img alt="img" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/7.jpg"></a>
-                                        </figure>
-                                        <div class="sm-single-gallery-info">
-                                            <a href="assets/img/gallery/big/7.jpg" data-fancybox-group="gallery" class="fancybox">
-                                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/plus.png" alt="plus icon img">
-                                            </a>
-                                        </div>
-                                    </div>
+                            </div>
+
+                            <div class="responsive">
+                                <div class="gallery">
+                                    <a target="_blank" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/7.jpg">
+                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/7.jpg"> alt="food" width="600" height="400">
+                                    </a>
+                                    <div class="desc">Add a description of the image here</div>
                                 </div>
-                                <!-- End single gallery image -->
-                                <!-- start single gallery image -->
-                                <div class="sm-single-gallery col-md-4 mix restaurant">
-                                    <div class="sm-single-gallery-item">
-                                        <figure class="sm-single-gallery-img">
-                                            <a href="#"><img alt="img" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/8.jpg"></a>
-                                        </figure>
-                                        <div class="sm-single-gallery-info">
-                                            <a href="assets/img/gallery/big/8.jpg" data-fancybox-group="gallery" class="fancybox">
-                                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/plus.png" alt="plus icon img">
-                                            </a>
-                                        </div>
-                                    </div>
+                            </div>
+
+                            <div class="responsive">
+                                <div class="gallery">
+                                    <a target="_blank" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/8.jpg">
+                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/8.jpg"> alt="food" width="600" height="400">
+                                    </a>
+                                    <div class="desc">Add a description of the image here</div>
                                 </div>
-                                <!-- End single gallery image -->
-                                <!-- start single gallery image -->
-                                <div class="sm-single-gallery col-md-4 mix dessert">
-                                    <div class="sm-single-gallery-item">
-                                        <figure class="sm-single-gallery-img">
-                                            <a href="#"><img alt="img" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gallery/small/9.jpg"></a>
-                                        </figure>
-                                        <div class="sm-single-gallery-info">
-                                            <a href="assets/img/gallery/big/9.jpg" data-fancybox-group="gallery" class="fancybox">
-                                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/plus.png" alt="plus icon img">
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- End single gallery image -->
                             </div>
                         </div>
                     </div>
