d623eab39ded0a41d348cece53891ff42c023c83
AhsanTalhaAulin <hoque.talha@gmail.com>
2021-09-08 22:52:22 +0600

    $commitMsg

--- php/home/services.php
+++ php/home/services.php
@@ -12,102 +12,105 @@
 
                     <div class="content">
                         <div class="abc">
-                        <div class="sm-tab-content-area">
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <div class="sm-tab-content-left">
-                                                    <ul class="sm-menu-item-nav">
-                                                        <li>
-                                                            <div class="media">
-                                                                <div class="media-left">
-                                                                    <a href="#">
-                                                                        <img class="media-object" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/menu/default.jpg" alt="img">
-                                                                    </a>
-                                                                </div>
-                                                                <div class="media-body">
-                                                                    <h4 class="media-heading"><a href="#">Private Catering</a></h4>
-                                                                    
-                                                                    <p>Dawats, Birthdays, Anniversaries, Rooftop Parties, Outdoor Sports, Picnics, Bridal Showers, Baby Showers, Milad, Iftar and ShehriParties.</p>
-                                                                </div>
-                                                            </div>
-                                                        </li>
-                                                        <li>
-                                                            <div class="media">
-                                                                <div class="media-left">
-                                                                    <a href="#">
-                                                                        <img class="media-object" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/menu/default.jpg" alt="img">
-                                                                    </a>
-                                                                </div>
-                                                                <div class="media-body">
-                                                                    <h4 class="media-heading"><a href="#">Corporate Catering </a></h4>
-                                                                    
-                                                                    <p>Office Parties, Training Programs & Workshops, Meetings & Seminars, Sales Conferences, AGMs, Board Meetings, Town Hall Meetings, Company Celebrations, Customer Nights, Product Launch, Roadshows, University Club Events, University Competitions, University Events, Convocations etc</p>
-                                                                </div>
-                                                            </div>
-                                                        </li>
-                                                        <li>
-                                                            <div class="media">
-                                                                <div class="media-left">
-                                                                    <a href="#">
-                                                                        <img class="media-object" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/menu/default.jpg" alt="img">
-                                                                    </a>
-                                                                </div>
-                                                                <div class="media-body">
-                                                                    <h4 class="media-heading"><a href="#">Wedding- Engagement</a></h4>
-                                                                    <p>Akhth, Aiburo Bhat, Sangeet, Gaye Holud, Wedding, Reception, Post-Marriage Parties, Anniversary etc.</p>
-                                                                </div>
-                                                            </div>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="sm-tab-content-right">
-                                                    <ul class="sm-menu-item-nav">
-                                                        <li>
-                                                            <div class="media">
-                                                                <div class="media-left">
-                                                                    <a href="#">
-                                                                        <img class="media-object" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/menu/default.jpg" alt="img">
-                                                                    </a>
-                                                                </div>
-                                                                <div class="media-body">
-                                                                    <h4 class="media-heading"><a href="#">Tour Operator</a></h4>
-                                                                    <p>Coming Soon...</p>
-                                                                </div>
-                                                            </div>
-                                                        </li>
-                                                        <li>
-                                                            <div class="media">
-                                                                <div class="media-left">
-                                                                    <a href="#">
-                                                                        <img class="media-object" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/menu/default.jpg" alt="img">
-                                                                    </a>
-                                                                </div>
-                                                                <div class="media-body">
-                                                                    <h4 class="media-heading"><a href="#">Photography</a></h4>
-                                                                    <p>Coming Soon...</p>
-                                                                </div>
-                                                            </div>
-                                                        </li>
-                                                        <li>
-                                                            <div class="media">
-                                                                <div class="media-left">
-                                                                    <a href="#">
-                                                                        <img class="media-object" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/menu/default.jpg" alt="img">
-                                                                    </a>
-                                                                </div>
-                                                                <div class="media-body">
-                                                                    <h4 class="media-heading"><a href="#">Event Management</a></h4>
-                                                                    <p>Coming Soon...</p>
-                                                                </div>
-                                                            </div>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                            </div>
+                            <div class="sm-tab-content-area">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="sm-tab-content-left">
+                                            <ul class="sm-menu-item-nav">
+                                                <li>
+                                                    <div class="media">
+                                                        <div class="media-left">
+                                                            <a href="#">
+                                                                <img class="media-object" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/menu/default.jpg" alt="img">
+                                                            </a>
+                                                        </div>
+                                                        <div class="media-body">
+                                                            <h4 class="media-heading"><a href="#">Private Catering</a></h4>
+
+                                                            <p>Dawats, Birthdays, Anniversaries, Rooftop Parties, Outdoor Sports, Picnics, Bridal Showers, Baby Showers, Milad, Iftar and ShehriParties.</p>
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                    <div class="media">
+                                                        <div class="media-left">
+                                                            <a href="#">
+                                                                <img class="media-object" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/menu/default.jpg" alt="img">
+                                                            </a>
+                                                        </div>
+                                                        <div class="media-body">
+                                                            <h4 class="media-heading"><a href="#">Corporate Catering </a></h4>
+
+                                                            <p>Office Parties, Training Programs & Workshops, Meetings & Seminars, Sales Conferences, AGMs, Board Meetings, Town Hall Meetings, Company Celebrations, Customer Nights, Product Launch, Roadshows, University Club Events, University Competitions, University Events, Convocations etc</p>
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                    <div class="media">
+                                                        <div class="media-left">
+                                                            <a href="#">
+                                                                <img class="media-object" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/menu/default.jpg" alt="img">
+                                                            </a>
+                                                        </div>
+                                                        <div class="media-body">
+                                                            <h4 class="media-heading"><a href="#">Wedding- Engagement</a></h4>
+                                                            <p>Akhth, Aiburo Bhat, Sangeet, Gaye Holud, Wedding, Reception, Post-Marriage Parties, Anniversary etc.</p>
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                            </ul>
                                         </div>
                                     </div>
+                                    <div class="col-md-6">
+                                        <div class="sm-tab-content-right">
+                                            <ul class="sm-menu-item-nav">
+                                                <li>
+                                                    <div class="media">
+                                                        <div class="media-left">
+                                                            <a href="#">
+                                                                <img class="media-object" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/menu/default.jpg" alt="img">
+                                                            </a>
+                                                        </div>
+                                                        <div class="media-body">
+                                                            <h4 class="media-heading"><a href="#">Tour Operator</a></h4>
+                                                            <p>Both as local and foreign tour operator, we are determined to provide a unique and satisfying travel experience.
+                                                                Our services includes- Sundarban Tour, Kuakata, Cox’s Bazar, Saintmartin, Sajek, Bandarban, Nilgiri- in Bangladesh. As a foreign Tour, we have experience in – India, Nepal and Saudi-Arabia tour.
+                                                                Our high qualified services, comfortable tour planning, guides with profound knowledge of local information and the affordable price will make your trip enjoyable and special.
+                                                                If you want to explore Bangladesh completely rich, we are here with experiences to make it memorable.</p>
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                    <div class="media">
+                                                        <div class="media-left">
+                                                            <a href="#">
+                                                                <img class="media-object" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/menu/default.jpg" alt="img">
+                                                            </a>
+                                                        </div>
+                                                        <div class="media-body">
+                                                            <h4 class="media-heading"><a href="#">Photography</a></h4>
+                                                            <p>Our expert photographer provides photography to all kinds of Program like - Birthdays, Anniversaries, Picnics, Office Parties, Training Programs & Workshops, Company Celebrations, Customer Nights, Gaye Holud, Wedding, Reception, Post-Marriage Parties, Anniversary etc.</p>
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                    <div class="media">
+                                                        <div class="media-left">
+                                                            <a href="#">
+                                                                <img class="media-object" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/menu/default.jpg" alt="img">
+                                                            </a>
+                                                        </div>
+                                                        <div class="media-body">
+                                                            <h4 class="media-heading"><a href="#">Event Management</a></h4>
+                                                            <p>Coming Soon...</p>
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
