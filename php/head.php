b93948a44a9de0db5ef9fbc9b69b66939ffbae0d
Ahsanul Hoque <hoque.talha@gmail.com>
2021-10-25 20:25:01 +0600

    $commitMsg

--- php/head.php
+++ php/head.php
@@ -142,4 +142,8 @@ catering service in dhaka
 
 <!-- Creator Studio -->
 <meta property="fb:pages" content="107468647781170" />
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap');
+</style> 
 </head>
