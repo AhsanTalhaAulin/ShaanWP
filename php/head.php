9b1a86ad86db9532c21e9f4026b918cf78d7d825
Ahsanul Hoque <hoque.talha@gmail.com>
2021-10-26 15:04:03 +0600

    $commitMsg

--- php/head.php
+++ php/head.php
@@ -146,4 +146,8 @@ catering service in dhaka
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap');
 </style> 
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Lobster&family=Patrick+Hand&display=swap');
+</style>
 </head>
