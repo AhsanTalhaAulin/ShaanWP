2f9d23123c56f2f07f3e23eaaa2b6f2aa1293d06
Ahsanul Hoque <hoque.talha@gmail.com>
2021-10-26 14:02:32 +0600

    $commitMsg

---
+++ php/google_adsense.php
@@ -0,0 +1,31 @@
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+  <!-- google adsense -->
+  <script data-ad-client="ca-pub-8150817687606301" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <!-- Global site tag (gtag.js) - Google Ads: 1007102543 -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=AW-1007102543"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'AW-1007102543');
+  </script>
+
+  <!-- Event snippet for Website sale conversion page -->
+  <script>
+    gtag('event', 'conversion', {
+      'send_to': 'AW-1007102543/O4DCCJ-DzPcCEM_UnOAD',
+      'transaction_id': ''
+    });
+  </script>
+
+<!-- Creator Studio -->
+<meta property="fb:pages" content="107468647781170" />
