ec640c525ec2e7b6960e47bf91bbbda2d5c3ab2f
Ahsanul Hoque <hoque.talha@gmail.com>
2021-10-26 14:32:41 +0600

    $commitMsg

--- php/head.php
+++ php/head.php
@@ -84,6 +84,8 @@ catering service in dhaka
   <title>Shaan Mughal Food Service</title>
 
 
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/favicon.ico" type="image/x-icon">
 
   <!-- Font awesome -->
   <link href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/css/font-awesome.css" rel="stylesheet">
@@ -105,9 +107,41 @@ catering service in dhaka
 
   <!-- Google Fonts -->
   <link href='https://fonts.googleapis.com/css?family=Tangerine' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Prata' rel='stylesheet' type='text/css'>
 
 
-  <?php include "google_adsense.php" ?>
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+  <!-- google adsense -->
+  <script data-ad-client="ca-pub-8150817687606301" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <!-- Global site tag (gtag.js) - Google Ads: 1007102543 -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=AW-1007102543"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'AW-1007102543');
+  </script>
+
+  <!-- Event snippet for Website sale conversion page -->
+  <script>
+    gtag('event', 'conversion', {
+      'send_to': 'AW-1007102543/O4DCCJ-DzPcCEM_UnOAD',
+      'transaction_id': ''
+    });
+  </script>
+
+<!-- Creator Studio -->
+<meta property="fb:pages" content="107468647781170" />
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Lobster&family=Patrick+Hand&display=swap');
